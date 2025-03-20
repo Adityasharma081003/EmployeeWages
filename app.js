fa4406420625eff6cdd060a4f68b44aa1e906fd7
Adityasharma081003 <rs6441419@gmail.com>
2025-03-20 14:16:57 +0530

    [Aditya] add:adding UC-7

--- app.js
+++ app.js
@@ -79,3 +79,44 @@ while(totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS){
 }
 empWage=calcDailyWage(totalEmpHrs);
 console.log("UC6-Total Days : "+totalWorkingDays+" Total Hrs : "+totalEmpHrs+" Emp Wage : "+empWage);
+let totEmpWage=0;
+function sum(dailyWage){
+    totEmpWage+=dailyWage;
+}
+empDailyWageArr.forEach(sum);
+console.log("UC7A-Total Days : "+totalWorkingDays+" Total Hrs : "+totalEmpHrs+" Emp Wage : "+totEmpWage);
+function totalWages(totalWage,dailyWage){
+    return totalWage+dailyWage;
+}
+console.log("UC7A-Emp Wage with reduce : "+empDailyWageArr.reduce(totalWages,0));
+let dailyCntr=0;
+function mapDayWithWage(dailyWage){
+    dailyCntr++;
+    return dailyCntr +" = "+ dailyWage;
+}
+let mapDayWithWageArr=empDailyWageArr.map(mapDayWithWage);
+console.log("UC7B-Daily Wage Map");
+console.log(mapDayWithWageArr);
+function fulltimeWage(dailyWage){
+    return dailyWage.includes("160");
+}
+let fullDayWageArr=mapDayWithWageArr.filter(fulltimeWage);
+console.log("UC7C-Daily Wage Filter When Fulltime Wage Earned");
+console.log(fullDayWageArr);
+function findFulltimeWage(dailyWage){
+    return dailyWage.includes("160");
+}
+console.log("UC7D-First time Fulltime wage was earned on Day : "+mapDayWithWageArr.find(findFulltimeWage));
+function isAllFulltimeWage(dailyWage){
+    return dailyWage.includes("160");
+}
+console.log("UC7E-Check All Elements have Full Time Wage : "+fullDayWageArr.every(isAllFulltimeWage));
+function isAnyPartTimeWage(dailyWage){
+    return dailyWage.includes("80");
+}
+console.log("UC7F-Check If Any Part Time Wage : "+mapDayWithWageArr.some(isAnyPartTimeWage));
+function totalDaysWorked(numOfDays,dailyWage){
+    if(dailyWage>0) return numOfDays+1;
+    return numOfDays;
+}
+console.log("UC7G-Number of Days Emp Worked : "+empDailyWageArr.reduce(totalDaysWorked,0));
