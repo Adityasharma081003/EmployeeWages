bd991c6d983fcc3b6d0786a2474dcf1b2e57a41b
Adityasharma081003 <rs6441419@gmail.com>
2025-03-20 13:48:15 +0530

    [Aditya] add: adding UC-4

--- app.js
+++ app.js
@@ -43,3 +43,11 @@ empCheck=Math.floor(Math.random()*10)%3;
 empHrs=getWorkingHours(empCheck);
 empWage=empHrs*WAGE_PER_HOUR;
 console.log("Emp Wage : "+empWage);
+const NUM_OF_WORKING_DAYS=2;
+emphrs=0;
+for(let day=0; day<NUM_OF_WORKING_DAYS; day++){
+    let empCheck = Math.floor(Math.random()*10)%3;
+    emphrs+=getWorkingHours(empCheck);
+}
+empWage=emphrs*WAGE_PER_HOUR;
+console.log("Total Hrs : "+emphrs+" Emp Wage : "+empWage);
